53cef1b2cafb64e78930fcf2a5cf4a9589f11a46
Jason Lu <jasonluchunyin2010@gmail.com>
2026-04-22 14:38:27 +0800

    Initial Commit

---
+++ DistractionFreeIOS/DistractionFreeIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DistractionFreeIOSApp.swift
+//  DistractionFreeIOS
+//
+//  Created by Aarav Morakhiya on 22/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct DistractionFreeIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
